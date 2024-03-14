e7ca4972dd57c29b606ee182303d7dbdb095acf1
yosh21 <Matthew.Glanville@mail.bcu.ac.uk>
2024-03-14 23:37:11 +0000

    updated the game version so it doesnt keep causing problems (had to re-sort out the textures)

--- Assets/C# scripts/Bacteria.cs
+++ Assets/C# scripts/Bacteria.cs
@@ -6,6 +6,8 @@ public class Bacteria : MonoBehaviour
 {
     [SerializeField] private float movespeed = 5;
     [SerializeField] private float health = 20;
+    [SerializeField] private GameObject Point1; 
+    private bool passedPoint1 = false; 
     private float xMove = 3;
     private float yMove = 0;
     // Start is called before the first frame update
@@ -16,7 +18,6 @@ public class Bacteria : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         gameObject.transform.position = gameObject.transform.position + new Vector3(xMove * movespeed * Time.deltaTime, yMove * movespeed * Time.deltaTime, 0);
         if (gameObject.transform.position.x > 3.5)
         {
